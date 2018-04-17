8bc6c64812bc3ed26a2b1b4b233cdad0dc59426f
beginor <beginor@qq.com>
2018-04-17 13:18:56 +0800

    switch to typescript.

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import alias from 'rollup-plugin-alias';
 import commonjs from 'rollup-plugin-commonjs';
 import css from 'rollup-plugin-css-only';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
 import uglify from 'rollup-plugin-uglify';
 
 // `npm run build` -> `production` is true
@@ -12,7 +13,7 @@ const production = !process.env.ROLLUP_WATCH;
 console.log('ROLLUP_WATCH: ' + process.env.ROLLUP_WATCH)
 
 export default {
-    input: './src/main.js',
+    input: './src/main.ts',
     output: {
         format: 'iife',
         file: './dist/bundle.js',
@@ -21,6 +22,9 @@ export default {
     treeshake: production,
     external: [],
     plugins: [
+        typescript({
+            tsconfig: 'tsconfig.json'
+        }),
         css({ output: './dist/bundle.css' }),
         alias({}),
         nodeResolve({ jsnext: true, module: true }),
