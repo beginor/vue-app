b8549b847cabccd374cf70c4915ce61a35b7c946
beginor <beginor@qq.com>
2021-12-06 12:10:51 +0800

    update packages.

--- copy-libs.sh
+++ copy-libs.sh
@@ -3,7 +3,7 @@ rm -rf dist/libs && mkdir -p dist/libs
 # tslib
 mkdir dist/libs/tslib
 cp -v node_modules/tslib/tslib.es6.js dist/libs/tslib/tslib.js
-npx rollup -c rollup/rollup.tslib.js
+pnpm exec rollup -c rollup/rollup.tslib.js
 # bootstrap-icons
 cp -rv node_modules/bootstrap-icons/icons dist/libs/bootstrap-icons
 # bootstrap
@@ -12,4 +12,4 @@ cp -v node_modules/bootstrap/dist/css/bootstrap.min.css dist/libs/bootstrap
 cp -v node_modules/bootstrap/dist/js/bootstrap.esm.js dist/libs/bootstrap
 cp -v node_modules/bootstrap/dist/js/bootstrap.esm.min.js dist/libs/bootstrap
 # @popperjs/core
-npx rollup -c rollup/rollup.popperjs.js
+pnpm exec rollup -c rollup/rollup.popperjs.js
