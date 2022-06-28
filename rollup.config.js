b6af038af59c74f6e7f46aa28ac05a70e2bb7470
beginor <beginor@qq.com>
2022-06-28 16:36:27 +0800

    fix app css output

--- rollup.config.js
+++ rollup.config.js
@@ -27,7 +27,7 @@ export default {
   plugins: [
     esbuild({ tsconfig: 'tsconfig.json', sourceMap: !production, minify: production, legalComments: 'none' }),
     scss({
-      output: 'dist/main.css', sass: require('sass'), sourceMap: !production,
+      output: 'dist/app.css', sass: require('sass'), sourceMap: !production,
       outputStyle: !production ? 'expanded' : 'compressed'
     }),
     alias({}),
