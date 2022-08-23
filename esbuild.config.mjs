b2b0437d92eb07e488ccaa0e7e8abe19cb295bff
beginor <beginor@qq.com>
2022-08-23 14:05:30 +0800

    update esbuild scripts

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,35 +1,9 @@
-import { build } from 'esbuild';
+import { createOptions, esbuild } from './esbuild.options.mjs';
 
-import options from './esbuild.options.mjs';
-
-const argv = process.argv;
-const watching = argv.indexOf('--watch') > -1;
-
-options.watch = !watching ? false : {
-  onRebuild(error, result) {
-    if (error) {
-      console.error('watch build failed: ');
-      console.error(JSON.stringify(error, undefined, 2));
-    }
-    else {
-      console.log('watch build succeeded: ');
-      console.error(JSON.stringify(result, undefined, 2));
-    }
-  }
-};
-
-const start = Date.now();
-
-build(options).then(result => {
-  const end = Date.now();
-  if (watching) {
-    console.log('watching ...');
-  }
-  else {
-    console.log(`build completed in ${end - start} ms`);
-    console.log(JSON.stringify(result, undefined, 2));
-  }
-}).catch(ex => {
-  console.error(ex);
-  process.exit(1);
-});
+const options = createOptions(
+  [
+    './src/main.ts'
+  ],
+  './dist/'
+);
+esbuild(options);
